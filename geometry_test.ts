ab80675d767bff5cf58dbbe7bb226b4be1312693
leotsx <leonardoilprofessore@gmail.com>
2026-03-22 16:19:18 +0100

    tasks

--- geometry_test.ts
+++ geometry_test.ts
@@ -1,6 +1,8 @@
 import { assertAlmostEquals } from "jsr:@std/assert@^1.0.0";
-import { Circle, Point2D } from "./geometry.ts";
+import { Circle, Point2D, Rectangle } from "./geometry.ts";
 
+
+// Circle Circumference
 Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
   // Given
   const circle = new Circle(new Point2D(3, 4), 5);
@@ -10,4 +12,29 @@ Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
 
   // Then
   assertAlmostEquals(actual, 31.416, 0.01);
+});
+
+// Circle Area
+Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
+  // Given
+  const circle = new Circle(new Point2D(3, 4), 5);
+
+  // When
+  const actual = circle.circumference();
+
+  // Then
+  assertAlmostEquals(actual, 31.416, 0.01);
+});
+
+
+// Rectangle Area
+Deno.test("area of a rectangle with width 5 and height 5 is roughly 25", () => {
+  // Given
+  const rectangle = new Rectangle(new Point2D(0, 0), new Point2D(5, 5));
+
+  // When
+  const actual = rectangle.area();
+
+  // Then
+  assertAlmostEquals(actual, 25, 0.01);
 });
